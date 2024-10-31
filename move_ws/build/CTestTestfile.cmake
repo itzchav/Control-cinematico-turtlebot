8a23ddc862810eb103048d091e248975a410775a
Karla Salado <113056744+itzchav@users.noreply.github.com>
2024-10-31 14:36:41 -0600

    Add files via upload

---
+++ move_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/karla/move_ws/src
+# Build directory: /home/karla/move_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mov_turtle")
+subdirs("prm_slam")
+subdirs("joystick_teleop")
+subdirs("rplidar_ros")
+subdirs("turtle_sim")
